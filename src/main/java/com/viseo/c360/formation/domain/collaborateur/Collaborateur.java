f127d159a8a0bad87cd56b62b69fd90febb3076a
AndroletViseo <thomas.lecomte@viseo.com>
2016-03-16 16:45:08 +0100

    debug annotation 'Valid' depuis pom.xml

--- src/main/java/com/viseo/c360/formation/domain/collaborateur/Collaborateur.java
+++ src/main/java/com/viseo/c360/formation/domain/collaborateur/Collaborateur.java
@@ -4,7 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Collaborateur {
@@ -15,8 +18,13 @@ public class Collaborateur {
 	@Version
 	long version;
 	
+	@NotNull
+	@NotBlank
 	@Size(min=2)
 	String matricule;
+	
+	@NotNull
+	@NotBlank
 	String nom;
 	String prenom;
 	
--- src/main/java/com/viseo/c360/formation/rest/CollaborateurWS.java
+++ src/main/java/com/viseo/c360/formation/rest/CollaborateurWS.java
@@ -1,10 +1,15 @@
 package com.viseo.c360.formation.rest;
 
-import javax.inject.Inject;
+import java.util.Set;
 
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,21 +20,34 @@ import com.viseo.c360.formation.domain.collaborateur.Collaborateur;
 
 
 @RestController
-@RequestMapping(value = "${endpoint.collaborateurs}")
+//@RequestMapping(value = "${endpoint.collaborateurs}")
 public class CollaborateurWS {
+	
+	/*
+	@Autowired
+	private Validator validator;
+	*/
 	
 	@Inject
 	CollaborateurDAO collaborateurDAO;
 	
 	@RequestMapping(method = RequestMethod.POST)
     @Transactional
-    public String addCollaborateur(@Validated @RequestBody Collaborateur myCollaborateur, BindingResult bindingResult){
+    public String addCollaborateur(@Valid @RequestBody Collaborateur myCollaborateur/*,BindingResult bindingResult*/){
 
+		//Set<ConstraintViolation<Collaborateur>> a = validator.validate(myCollaborateur, Collaborateur.class);
+		
+		
+		System.out.println("COUCOU" + myCollaborateur.getMatricule());
+		System.out.println("COUCOU" + myCollaborateur.getNom());
+		
+		/*
 		//check var sent !
 		if(!(bindingResult.hasErrors())){
 			//persist the data
 			collaborateurDAO.addCollaborateur(myCollaborateur);
 		}
+		*/
         return "collaborateur ajouté !";
     }
 //	
