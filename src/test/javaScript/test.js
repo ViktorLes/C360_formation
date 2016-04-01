b32a97834f02db5e5d5fe50baf056fcaf3c1e340
AndroletViseo <thomas.lecomte@viseo.com>
2016-04-01 18:08:46 +0200

    first test obvious

--- src/test/javaScript/test.js
+++ src/test/javaScript/test.js
@@ -1,18 +1,7 @@
-describe('products category service', function () {
-  it('should return the expected categories', function () {
-    angular.mock.module('products');
+describe('first tests !', function () {
+  it('should works because 3 = 3', function () {
 
-    var service;
-    
-    // Get the service from the injector
-    angular.mock.inject(function GetDependencies(CategoryService) {
-      service = CategoryService;
-    });
-
-    // call the function on our service instance
-    var categories = service.getCategories();
-
-    expect(categories).toEqual({ 1: 'Beverages', 2: 'Condiments' });
+    expect(4).toBe(3);
   });
 });
 
