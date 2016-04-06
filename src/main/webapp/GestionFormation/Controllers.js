62aac8ec130dd3c1127bfacde2affdbf2bb8e116
AndroletViseo <thomas.lecomte@viseo.com>
2016-04-06 09:51:22 +0200

    modif dependencies 50%

--- src/main/webapp/GestionFormation/Controllers.js
+++ src/main/webapp/GestionFormation/Controllers.js
@@ -120,22 +120,22 @@
 //				    }
 //				  ];
 
-				  function getDayClass(data) {
-				    var date = data.date,
-				      mode = data.mode;
-				    if (mode === 'day') {
-				      var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-				      for (var i = 0; i < $scope.events.length; i++) {
-				        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-				        if (dayToCheck === currentDay) {
-				          return $scope.events[i].status;
-				        }
-				      }
-				    }
-
-				    return '';
-				  }
+//				  function getDayClass(data) {
+//				    var date = data.date,
+//				      mode = data.mode;
+//				    if (mode === 'day') {
+//				      var dayToCheck = new Date(date).setHours(0,0,0,0);
+//
+//				      for (var i = 0; i < $scope.events.length; i++) {
+//				        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+//
+//				        if (dayToCheck === currentDay) {
+//				          return $scope.events[i].status;
+//				        }
+//				      }
+//				    }
+//
+//				    return '';
+//				  }
 				});
 		
