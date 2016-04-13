93992af53ce9bbba1089f6dae0cddff74f44e5d8
Kalmouni <NKA3484@viseo.net>
2016-04-13 11:19:46 +0200

    test02

--- src/test/javaScript/test.js
+++ src/test/javaScript/test.js
@@ -1,11 +1,18 @@
-describe('first tests !', function () {
-  it('should works because 3 = 3', function () {
+//describe('first tests !', function () {
+  //it('should works because 3 = 3', function () {
 
-    expect(3).toBe(4);
+    //expect(4).toBe(4);
+  //});
+
+//});
+
+describe('GF1', function() {
+
+  beforeEach(module('routeApp'));
+  beforeEach(module('ngMockE2E'));
+  it('should FILTER RESULTS', function() {
+   // element(by.model('EC.collaborateur.matricule')).sendKeys('');
+   // element(by.css('btn btn-primary')).click();
+    element ();
   });
-
 });
-
-
-
-
