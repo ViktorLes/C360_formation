6df8aec6d511362fd2904276400669aca4089455
AndroletViseo <thomas.lecomte@viseo.com>
2016-04-13 19:27:11 +0200

    beforeEach lecture du HTML

--- src/test/javaScript/test.js
+++ src/test/javaScript/test.js
@@ -1,8 +1,32 @@
 describe('first tests !', function () {
+
+  beforeEach(module('routeApp'));
+  //beforeEach(module('ngMock'));
+
+  var ctrl;
+  var scope;
+  var element;
+
+  var form;
+
+  var backend;
+
+
+  beforeEach(inject(function($controller, $httpBackend, $compile) {
+    backend = $httpBackend;
+    ctrl = $controller('MainCtrl');
+
+    var html = null;//get template in var template = ...;
+    var template = angular.element(html);
+    scope = template.scope();
+    var linkFn = $compile(template);
+    element = linkFn(scope);
+
+    //search the form directive in 'element'
+
+  }));
+
   it('should works because 3 = 3', function () {
-
-    ctrl.actionEnregistrer()
-
     
     expect(4).toBe(4);
   });
