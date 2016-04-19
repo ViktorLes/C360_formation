7c88231e8813c51e6e82f24cb36d3b7a27b051f6
AndroletViseo <thomas.lecomte@viseo.com>
2016-04-19 18:44:14 +0200

    reprise des tests

--- src/test/javaScript/test.js
+++ src/test/javaScript/test.js
@@ -1,17 +1,6 @@
-
-//describe('first tests !', function () {
-  //it('should works because 3 = 3', function () {
-    //expect(4).toBe(4);
-  //});
-
-//});
-
 describe('GF1', function() {
   var ctrl;
   var backend;
-  var scope;
-  var element;
-  var  form;
 
   beforeEach(module('routeApp'));
   beforeEach(module('ngMock'));
@@ -19,32 +8,13 @@ describe('GF1', function() {
 
   beforeEach(inject(function ($controller, $httpBackend, $http, $templateCache){
     backend = $httpBackend;
-   // backend.whenGET(/^templates\//).passThrough();
-
     ctrl = $controller('CtrlCol');
-
-    /*
-     *     var html = $templateCache.get('templates/EnregistrementCollaborateur.html');
-    backend.flush();
-    var template = angular.element(html);
-    scope = template.scope();
-    var linkFn = $compile(template);
-    element = linkFn(scope);
-    form = element.find("form");
-     */
-    
-   //$http.get('templates/EnregistrementCollaborateur.html').then(function(html) {
-    var html = $templateCache.get('templates/EnregistrementCollaborateur.html');
-    //var template = angular.element($templateCache.get('templates/EnregistrementCollaborateur.html')[1]);
-      scope = ctrl.scope;
-      var linkFn = $compile(template);
-      element = linkFn(scope);
-      form = element.find("form");
     }));
 
  
 
   describe('Test Enregistrement',function () {
+
     beforeEach(function () {
       ctrl.collaborateur = {};
       ctrl.collaborateur.nom = "Henri";
@@ -54,13 +24,14 @@ describe('GF1', function() {
 
 
 
-    it('Test Valide', function () {
+    it('Valide', function () {
       ctrl.actionEnregistrer();
       backend.expectPOST('api/collaborateurs').respond('true');
       backend.flush();
       expect(ctrl.isNewMatricule).toBeTruthy();
     });
-    it('Test Invalide', function () {
+
+    it('Invalide', function () {
       ctrl.actionEnregistrer();
       backend.expectPOST('api/collaborateurs').respond('false');
       backend.flush();
