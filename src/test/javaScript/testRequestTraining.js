4c619b7f421aa28ea115953a48c2b994e83b0998
AndroletViseo <thomas.lecomte@viseo.com>
2016-05-10 18:13:54 +0200

    test 3, 4 & 5

--- src/test/javaScript/testRequestTraining.js
+++ src/test/javaScript/testRequestTraining.js
@@ -25,37 +25,43 @@ describe('Demande de Formation', function() {
         expect(ctrl.training).toEqual([{id:1,version:0,trainingTitle:"AngularJS",numberHalfDays:1},{id:6,version:0,trainingTitle:"Hibernate",numberHalfDays:8}]);
     });
 
-    it('2) Demande de formation sans session planifié', function(){
+    it('2) Demande de formation sans sessions planifiées', function(){
         ctrl.requestedTrainingId = 6;
         ctrl.loadTrainingSessions();
         backend.expectGET('api/formations/6/sessions').respond([]);
         backend.flush();
         expect(ctrl.isListEmpty).toBeTruthy();
         ctrl.verifyForm();
-        backend.expectPOST('api/requests').respond({training: 6, collaborator: 4, trainingSessions:[]});
+        backend.expectPOST('api/requests', {training: 6, collaborator: 4, trainingSessions:[]}).respond(true);
         backend.flush();
     });
 
-    it(' Demande de formation sans selection au préalable', function() {
-       // expect(ctrl.listTrainingSession).toEqual([]);
-        /*
-         ctrl.saveAction();
-
-         backend.expectPOST('api/collaborateurs').respond('true');
-         backend.flush();
-         expect(ctrl.isNewPersonalIdNumber).toBeTruthy();
-         //expect(loc.path()).toEqual('pageblanche.html');
-         */
+    it('3) Demande de formation avec sessions sélectionnées', function () {
+        ctrl.requestedTrainingId = 1;
+        ctrl.loadTrainingSessions();
+        backend.expectGET('api/formations/1/sessions').respond('[{"id":2,"version":0,"training":{"id":1,"version":0,"trainingTitle":"AngularJS","numberHalfDays":1},"beginning":1462341600000,"ending":1462514400000,"location":"Salle Phuket"},{"id":3,"version":0,"training":{"id":1,"version":0,"trainingTitle":"AngularJS","numberHalfDays":1},"beginning":1462600800000,"ending":1462860000000,"location":"Salle Phuket"}]');
+        backend.flush();
+        expect(ctrl.isListEmpty).toBeFalsy();
+        ctrl.listTrainingSession[1].isChecked = true;
+        ctrl.verifyForm();
+        backend.expectPOST('api/requests', {training: 1, collaborator: 4, trainingSessions:[3]}).respond(true);
+        backend.flush();
     });
 
-    it('Demande de formation avec session', function () {
-        /*
-         ctrl.saveAction();
+    it('4) Demande de formation sans selection au préalable', function() {
+        expect(ctrl.listTrainingSession).toBeUndefined();
+        expect(ctrl.requestedTrainingId).toBeUndefined();
+        ctrl.verifyForm();
+        expect(ctrl.hasToChooseOneTraining).toBeTruthy();
+    });
 
-         backend.expectPOST('api/collaborateurs').respond('true');
-         backend.flush();
-         expect(ctrl.isNewPersonalIdNumber).toBeTruthy();
-         //expect(loc.path()).toEqual('pageblanche.html');
-         */
+    it('5) Demande de formation sans sessions sélectionnées', function () {
+        ctrl.requestedTrainingId = 1;
+        ctrl.loadTrainingSessions();
+        backend.expectGET('api/formations/1/sessions').respond('[{"id":2,"version":0,"training":{"id":1,"version":0,"trainingTitle":"AngularJS","numberHalfDays":1},"beginning":1462341600000,"ending":1462514400000,"location":"Salle Phuket"},{"id":3,"version":0,"training":{"id":1,"version":0,"trainingTitle":"AngularJS","numberHalfDays":1},"beginning":1462600800000,"ending":1462860000000,"location":"Salle Phuket"}]');
+        backend.flush();
+        expect(ctrl.isListEmpty).toBeFalsy();
+        ctrl.verifyForm();
+        expect(ctrl.noneSessionSelected).toBeTruthy();
     });
 });
