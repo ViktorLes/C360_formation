6c8090e3a64cedf30f39528f1a5dbfca99fc83bf
AndroletViseo <thomas.lecomte@viseo.com>
2016-05-12 16:31:21 +0200

    exception on object not found in bdd added

--- src/main/java/com/viseo/c360/formation/exception/PersistentObjectNotFoundException.java
+++ src/main/java/com/viseo/c360/formation/exception/PersistentObjectNotFoundException.java
@@ -3,6 +3,6 @@ package com.viseo.c360.formation.exception;
 public class PersistentObjectNotFoundException extends Exception{
     public PersistentObjectNotFoundException(){}
     public PersistentObjectNotFoundException(long id, String type){
-        super("No one "+type+" with id : '"+Long.toString(id)+"' has been found.");
+        super("No one "+type+" with id : '"+Long.toString(id)+"' has been found in database.");
     }
 }
