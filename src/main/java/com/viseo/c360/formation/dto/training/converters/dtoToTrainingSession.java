f647ec6c00b69c71f1591b19b5af3d96d3c0be91
AndroletViseo <thomas.lecomte@viseo.com>
2016-05-12 16:31:04 +0200

    exception on object not found in bdd added

--- src/main/java/com/viseo/c360/formation/dto/training/converters/dtoToTrainingSession.java
+++ src/main/java/com/viseo/c360/formation/dto/training/converters/dtoToTrainingSession.java
@@ -4,8 +4,7 @@ import com.viseo.c360.formation.dao.TrainingDAO;
 import com.viseo.c360.formation.domain.training.Training;
 import com.viseo.c360.formation.domain.training.TrainingSession;
 import com.viseo.c360.formation.dto.training.TrainingSessionDTO;
-import org.hibernate.Hibernate;
-import org.hibernate.HibernateException;
+import com.viseo.c360.formation.exception.PersistentObjectNotFoundException;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.Converter;
@@ -15,20 +14,20 @@ import javax.inject.Inject;
 public class DtoToTrainingSession implements Converter<TrainingSessionDTO, TrainingSession> {
     @Inject
     TrainingDAO trainingDAO;
-
     public TrainingSession convert(TrainingSessionDTO dto) {
         TrainingSession domain = new TrainingSession();
         try{
             Training training = trainingDAO.getTraining(dto.getTraining());
-        }catch(){
+            if(training == null) throw new PersistentObjectNotFoundException(dto.getTraining(), "TrainingSession");
+            domain.setTraining(training);
+        }catch(Exception e) {
             throw new ConversionFailedException(
                     TypeDescriptor.valueOf(TrainingSessionDTO.class),
                     TypeDescriptor.valueOf(TrainingSession.class),
                     dto,
-                    new Throwable("no TrainingSession with id : '"+Long.toString(dto.getTraining())+"' has been found.")
+                    new Throwable(e.getMessage())
             );
         }
-        domain.setTraining(training);
         return domain;
     }
 }
---
+++ src/main/java/com/viseo/c360/formation/exception/PersistentObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.viseo.c360.formation.exception;
+
+public class PersistentObjectNotFoundException extends Exception{
+    public PersistentObjectNotFoundException(){}
+    public PersistentObjectNotFoundException(long id, String type){
+        super("No one "+type+" with id : '"+Long.toString(id)+"' has been found.");
+    }
+}
