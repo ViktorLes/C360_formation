20f0abaa44e3495b622c11e6bf17d4032eda5e11
El Khadir <JEL3503@viseo.net>
2016-06-06 15:50:55 +0200

    add converter listTrainings to ListDescription

---
+++ src/main/java/com/viseo/c360/formation/converters/training/ListTrainingsToListDescription.java
@@ -0,0 +1,20 @@
+package com.viseo.c360.formation.converters.training;
+
+import com.viseo.c360.formation.domain.training.Training;
+import com.viseo.c360.formation.dto.training.TrainingDescription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTrainingsToListDescription {
+    public ListTrainingsToListDescription() {
+    }
+
+    public List<TrainingDescription> convert(List<Training> trainings) {
+        List<TrainingDescription> listDescription = new ArrayList<TrainingDescription>();
+        for(Training myTraining : trainings){
+            listDescription.add(new TrainingToDescription().convert(myTraining));
+        }
+        return listDescription;
+    }
+}
