61f5874f44d05d8be9e311472d8cbefa32bff2d4
El Khadir <JEL3503@viseo.net>
2016-06-07 10:56:49 +0200

    refactor RequestTrainingDescription

--- src/main/java/com/viseo/c360/formation/dto/collaborator/RequestTrainingDescription.java
+++ src/main/java/com/viseo/c360/formation/dto/collaborator/RequestTrainingDescription.java
@@ -7,33 +7,40 @@ import com.viseo.c360.formation.dto.training.TrainingDescription;
 import com.viseo.c360.formation.dto.training.TrainingSessionDescription;
 
 
-public class RequestTrainingDescription extends BaseDTO{
+public class RequestTrainingDescription extends BaseDTO {
 
 
-	TrainingDescription trainingDescription;
+    TrainingDescription trainingDescription;
 
-	List<TrainingSessionDescription> trainingSessionsDtos;
+    List<TrainingSessionDescription> trainingSessionsDescription;
 
-	CollaboratorDescription collaboratorDescription;
+    CollaboratorIdentity collaboratorIdentity;
 
-	public RequestTrainingDescription() {
-		super();
-	}
+    public RequestTrainingDescription() {
+        super();
+    }
 
-	public TrainingDescription getTrainingDescription() {
-		return trainingDescription;
-	}
-	public void setTrainingDescription(TrainingDescription trainingDescription) {
-		this.trainingDescription = trainingDescription;
-	}
-	public List<TrainingSessionDescription> getTrainingSessionsDescriptions() {
-		return trainingSessionsDtos;
-	}
-	public void setTrainingSessionsDtos(List<TrainingSessionDescription> trainingSessions) {
-		this.trainingSessionsDtos = trainingSessions;
-	}
-	public CollaboratorDescription getCollaboratorDescription() { return collaboratorDescription; }
-	public void setCollaborator(CollaboratorDescription collaboratorDescription) {
-		this.collaboratorDescription = collaboratorDescription;
-	}
+    public TrainingDescription getTrainingDescription() {
+        return trainingDescription;
+    }
+
+    public void setTrainingDescription(TrainingDescription trainingDescription) {
+        this.trainingDescription = trainingDescription;
+    }
+
+    public List<TrainingSessionDescription> getTrainingSessionsDescription() {
+        return trainingSessionsDescription;
+    }
+
+    public void setTrainingSessionsDescription(List<TrainingSessionDescription> trainingSessionsDescription) {
+        this.trainingSessionsDescription = trainingSessionsDescription;
+    }
+
+    public CollaboratorIdentity getCollaboratorIdentity() {
+        return collaboratorIdentity;
+    }
+
+    public void setCollaboratorIdentity(CollaboratorIdentity collaboratorIdentity) {
+        this.collaboratorIdentity = collaboratorIdentity;
+    }
 }
