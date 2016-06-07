a69a700003fd39eb6528930a9097cd0914d6667d
El Khadir <JEL3503@viseo.net>
2016-06-07 11:58:40 +0200

    refactor 100% (affectTrainingSpec.js missing

--- src/main/webapp/app/controller/affectTrainingSession.js
+++ src/main/webapp/app/controller/affectTrainingSession.js
@@ -5,74 +5,90 @@
 //************************************************************************************//
 angular.module('controllers').controller('controllerAffectTraining', ['$http', '$location', '$filter', '$timeout', function ($http, $location, $filter, $timeout) {
 
-    var self = this;
-    self.isCollabaratorListUpdated = false;
-    self.boolErrNoSessionSelected = false;
+        var self = this;
+        self.isCollabaratorListUpdated = false;
+        self.boolErrNoSessionSelected = false;
 
-    //Récupérer la liste des sessions disponible
-    $http.get("api/sessions").then(function (data) {
-        self.trainingSessionList = data.data;
-    });
-
-    self.displayTrainingSession = function(mySession){
-        return mySession.trainingDescription.trainingTitle+' - '+mySession.beginning+' à '+mySession.ending+' - '+mySession.location;
-    };
-
-    //déplace d'une liste à une autre
-    self.moveItem = function (item, from, to) {
-        var idx = from.indexOf(item);
-        if (idx != -1) {
-            from.splice(idx, 1);
-            to.push(item);
-        }
-    };
-
-    self.verifyForm = function () {
-        if (self.selectedSession !== undefined && self.selectedSession !== "") {
-            if (self.sessionSelected) {
-                self.saveAction();
-                self.setConfirmationMessageTimOut();
-            }
-        }
-        else {
-            self.boolErrNoSessionSelected = true;
-        }
-    };
-
-    self.saveAction = function () {
-        $http.put("api/sessions/" + self.sessionSelected.id + "/collaborators", self.selectedCollaboratorList).then(function (response) {
-            if(response.data) {
-                self.isCollabaratorListUpdated = true; }
+        //Récupérer la liste des sessions disponible
+        $http.get("api/sessions").then(function (data) {
+            self.trainingSessionList = data.data;
         });
-    };
 
-    self.setConfirmationMessageTimOut = function () {
-        $timeout(function () {
-            self.isCollabaratorListUpdated = false;
-        }, 3000);
-    };
-
-    self.selectSessionObjectFromInputText = function () {
-        var selectedSessionSplittedArray = self.selectedSession.split(/ - | à /);
-        self.trainingSessionObject = {
-            beginning: selectedSessionSplittedArray[1],
-            ending: selectedSessionSplittedArray[2],
-            location: selectedSessionSplittedArray[3],
-            trainingDescription:{
-                trainingTitle:selectedSessionSplittedArray[0]
+        //Récupérer la liste des collaborateurs affectés et non affectés à la session
+        self.loadNotAffectedCollaboratorsList = function () {
+            self.availableCollaboratorList = [];
+            self.selectedCollaboratorList = [];
+            self.boolErrNoSessionSelected = false;
+            self.sessionSelected = self.selectSessionObjectFromInputText();
+            if (self.sessionSelected) {
+                $http.get("api/sessions/" + self.sessionSelected.id + "/collaboratorsnotaffected").then(function (data) {
+                    self.availableCollaboratorList = data.data;
+                });
+                $http.get("api/sessions/" + self.sessionSelected.id + "/collaboratorsaffected").then(function (data) {
+                    self.selectedCollaboratorList = data.data;
+                });
             }
         };
-        console.log("self.trainingSessionObject: ",self.trainingSessionObject);
-        return self.trainingSessionList.find(function(elem){
-            if(elem.beginning===self.trainingSessionObject.beginning &&
-               elem.ending===self.trainingSessionObject.ending &&
-               elem.location===self.trainingSessionObject.location &&
-               elem.trainingDescription.trainingTitle===self.trainingSessionObject.trainingDescription.trainingTitle){
-               return elem;
+
+        self.displayTrainingSession = function (mySession) {
+            return mySession.trainingDescription.trainingTitle + ' - ' + mySession.beginning + ' à ' + mySession.ending + ' - ' + mySession.location;
+        };
+
+        //déplace d'une liste à une autre
+        self.moveItem = function (item, from, to) {
+            var idx = from.indexOf(item);
+            if (idx != -1) {
+                from.splice(idx, 1);
+                to.push(item);
             }
-        });
-    }
-}])
+        };
+
+        self.verifyForm = function () {
+            if (self.selectedSession !== undefined && self.selectedSession !== "") {
+                if (self.sessionSelected) {
+                    self.saveAction();
+                    self.setConfirmationMessageTimOut();
+                }
+            }
+            else {
+                self.boolErrNoSessionSelected = true;
+            }
+        };
+
+        self.saveAction = function () {
+            $http.put("api/sessions/" + self.sessionSelected.id + "/collaborators", self.selectedCollaboratorList).then(function (response) {
+                if (response.data) {
+                    self.isCollabaratorListUpdated = true;
+                }
+            });
+        };
+
+        self.setConfirmationMessageTimOut = function () {
+            $timeout(function () {
+                self.isCollabaratorListUpdated = false;
+            }, 3000);
+        };
+
+        self.selectSessionObjectFromInputText = function () {
+            var selectedSessionSplittedArray = self.selectedSession.split(/ - | à /);
+            self.trainingSessionObject = {
+                beginning: selectedSessionSplittedArray[1],
+                ending: selectedSessionSplittedArray[2],
+                location: selectedSessionSplittedArray[3],
+                trainingDescription: {
+                    trainingTitle: selectedSessionSplittedArray[0]
+                }
+            };
+            return self.trainingSessionList.find(function (elem) {
+                if (elem.beginning === self.trainingSessionObject.beginning &&
+                    elem.ending === self.trainingSessionObject.ending &&
+                    elem.location === self.trainingSessionObject.location &&
+                    elem.trainingDescription.trainingTitle === self.trainingSessionObject.trainingDescription.trainingTitle) {
+                    return elem;
+                }
+            });
+        }
+    }])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/AffectTraining', {
