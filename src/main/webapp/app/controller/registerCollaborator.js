736f9a234a403dff28fcbf39452feedc5f9c5ee0
Lecomte <TLE3473@viseo.net>
2016-06-17 17:57:03 +0200

    error input messages managed right (enter keypress missing)

--- src/main/webapp/app/controller/registerCollaborator.js
+++ src/main/webapp/app/controller/registerCollaborator.js
@@ -13,6 +13,10 @@ angular.module('controllers')
             self.regex.firstName = new RegExp(data.data.FIRST_NAME);
         });
 
+        self.isErrorInputMessageDisplayed = function(inputForm, focus){
+            return !inputForm.$error.required && inputForm.$invalid && !focus;
+        };
+
         self.verifyForm = function (collaboratorForm) {
             if (collaboratorForm.$error.required) {
                     self.isThereAnEmptyField = true;
