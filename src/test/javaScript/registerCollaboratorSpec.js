e4500033343d56acfaea91717577f568b21f8e75
Lecomte <TLE3473@viseo.net>
2016-06-23 12:02:50 +0200

    test regex register collaborator OK

--- src/test/javaScript/registerCollaboratorSpec.js
+++ src/test/javaScript/registerCollaboratorSpec.js
@@ -2,6 +2,7 @@ describe('Enregistrement Collaborateur', function () {
     var ctrl;
     var backend;
     var loc;
+    var form;
 
     beforeEach(module('App'));
 
@@ -10,6 +11,13 @@ describe('Enregistrement Collaborateur', function () {
         loc = $location;
         loc.url('/RegisterCollaborator');
         ctrl = $controller('controllerRegisterCollaborator');
+        form = {
+            lastName: {$invalid: true,$error:{required:true}},
+            firstName: {$invalid: true,$error:{required:true}},
+            personnalIdNumber: {$invalid: true,$error:{required:true}},
+            $invalid: true,
+            $error: {required: [{}, {}, {}]}
+        };
     }));
 
     describe('Test EnregistrementCollaborateur', function () {
@@ -17,9 +25,6 @@ describe('Enregistrement Collaborateur', function () {
         beforeEach(function () {
             backend.expectGET('api/collaborateurs/regex').respond('{"PERSONNAL_ID_NUMBER":"[A-Z]{3}[0-9]{4}","LAST_NAME":"^[a-zA-Z-\'. áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+$","FIRST_NAME":"^[a-zA-Z-\'. áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]+$"}');
             ctrl.collaborator = {};
-            ctrl.collaborator.lastName = "Darmet";
-            ctrl.collaborator.firstName = "Henri";
-            ctrl.collaborator.personnalIdnumber = "HDA1234";
             backend.flush();
         });
 
@@ -29,16 +34,60 @@ describe('Enregistrement Collaborateur', function () {
         });
 
         it('Valide', function () {
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,true)).toBeFalsy();
+            ctrl.collaborator.lastName = "Darmet";
+            expect(ctrl.collaborator.lastName).toMatch(ctrl.regex.lastName);
+            form.$error.required=[{},{}];
+            form.lastName.$error={};
+            form.lastName.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,false)).toBeFalsy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,true)).toBeFalsy();
+            ctrl.collaborator.firstName = "Henri";
+            expect(ctrl.collaborator.firstName).toMatch(ctrl.regex.firstName);
+            form.$error.required=[{}];
+            form.firstName.$error={};
+            form.firstName.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,false)).toBeFalsy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,true)).toBeFalsy();
+            ctrl.collaborator.personnalIdNumber = "HDA1234";
+            expect(ctrl.collaborator.personnalIdNumber).toMatch(ctrl.regex.personnalIdNumber);
+            form.$error = {};
+            form.personnalIdNumber.$error={};
+            form.personnalIdNumber.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,false)).toBeFalsy();
+            form.$invalid=false;
             backend.expectPOST('api/collaborateurs').respond('true');
-            ctrl.saveAction();
+            ctrl.verifyForm(form);
             backend.flush();
             expect(ctrl.isNewPersonalIdNumber).toBeTruthy();
             expect(loc.path()).toBe('/pageblanche');
         });
 
         it('Invalide', function () {
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,true)).toBeFalsy();
+            ctrl.collaborator.lastName = "Darmet";
+            expect(ctrl.collaborator.lastName).toMatch(ctrl.regex.lastName);
+            form.$error.required=[{},{}];
+            form.lastName.$error={};
+            form.lastName.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,false)).toBeFalsy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,true)).toBeFalsy();
+            ctrl.collaborator.firstName = "Henri";
+            expect(ctrl.collaborator.firstName).toMatch(ctrl.regex.firstName);
+            form.$error.required=[{}];
+            form.firstName.$error={};
+            form.firstName.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,false)).toBeFalsy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,true)).toBeFalsy();
+            ctrl.collaborator.personnalIdNumber = "HDA1234";
+            expect(ctrl.collaborator.personnalIdNumber).toMatch(ctrl.regex.personnalIdNumber);
+            form.$error = {};
+            form.personnalIdNumber.$error={};
+            form.personnalIdNumber.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,false)).toBeFalsy();
+            form.$invalid=false;
             backend.expectPOST('api/collaborateurs').respond('false');
-            ctrl.saveAction();
+            ctrl.verifyForm(form);
             backend.flush();
             expect(ctrl.isNewPersonalIdNumber).toBeFalsy();
             expect(loc.path()).toBe('/RegisterCollaborator');
