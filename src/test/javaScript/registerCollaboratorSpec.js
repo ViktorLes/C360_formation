d020b67dca3e25a5a77a3a16f4349a2286b02666
Lecomte <TLE3473@viseo.net>
2016-06-23 12:17:47 +0200

    test regex register collaborator if not ok

--- src/test/javaScript/registerCollaboratorSpec.js
+++ src/test/javaScript/registerCollaboratorSpec.js
@@ -63,7 +63,7 @@ describe('Enregistrement Collaborateur', function () {
             expect(loc.path()).toBe('/pageblanche');
         });
 
-        it('Invalide', function () {
+        it('Invalide because of matricule', function () {
             expect(ctrl.isErrorInputMessageDisplayed(form.lastName,true)).toBeFalsy();
             ctrl.collaborator.lastName = "Darmet";
             expect(ctrl.collaborator.lastName).toMatch(ctrl.regex.lastName);
@@ -77,6 +77,31 @@ describe('Enregistrement Collaborateur', function () {
             form.$error.required=[{}];
             form.firstName.$error={};
             form.firstName.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,false)).toBeFalsy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,true)).toBeFalsy();
+            ctrl.collaborator.personnalIdNumber = "HDA1234";
+            expect(ctrl.collaborator.personnalIdNumber).toMatch(ctrl.regex.personnalIdNumber);
+            form.$error = {};
+            form.personnalIdNumber.$error={};
+            form.personnalIdNumber.$invalid=false;
+            expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,false)).toBeFalsy();
+            form.$invalid=false;
+            backend.expectPOST('api/collaborateurs').respond('false');
+            ctrl.verifyForm(form);
+            backend.flush();
+            expect(ctrl.isNewPersonalIdNumber).toBeFalsy();
+            expect(loc.path()).toBe('/RegisterCollaborator');
+        });
+        it('Invalide because of input', function () {
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,true)).toBeFalsy();
+            ctrl.collaborator.lastName = "Darmet@";
+            expect(ctrl.collaborator.lastName).not.toMatch(ctrl.regex.lastName);
+            form.$error.required=[{},{}];
+            form.lastName.$error={pattern:true};
+            form.lastName.$invalid=true;
+            expect(ctrl.isErrorInputMessageDisplayed(form.lastName,false)).toBeTruthy();
+            expect(ctrl.isErrorInputMessageDisplayed(form.firstName,true)).toBeFalsy();
+            ctrl.collaborator.firstName = "";
             expect(ctrl.isErrorInputMessageDisplayed(form.firstName,false)).toBeFalsy();
             expect(ctrl.isErrorInputMessageDisplayed(form.personnalIdNumber,true)).toBeFalsy();
             ctrl.collaborator.personnalIdNumber = "HDA1234";
