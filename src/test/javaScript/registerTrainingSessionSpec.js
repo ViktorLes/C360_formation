88f7b0c81ec3e03c9cd6150269f929f499bc6081
Lecomte <TLE3473@viseo.net>
2016-06-27 11:51:38 +0200

    test regex GF5

--- src/test/javaScript/registerTrainingSessionSpec.js
+++ src/test/javaScript/registerTrainingSessionSpec.js
@@ -27,7 +27,11 @@ describe('registerTrainingSession', function () {
     it('1) Enregistrement session', function () {
         ctrl.training = TRAININGS[0];
         ctrl.d1.dt = new Date(2016, 5, 23);
+        var dateBeginning=filter('date')(ctrl.d1.dt, "dd/MM/yyyy");
+        expect(dateBeginning).toMatch(ctrl.regex.beginning);
         ctrl.d2.dt = new Date(2016, 5, 25);
+        var dateEnding=filter('date')(ctrl.d2.dt, "dd/MM/yyyy");
+        expect(dateEnding).toMatch(ctrl.regex.ending);
         ctrl.beginningHour = "08:00";
         ctrl.endHour = "08:00";
         var meetingRoom1 = {name: 'Salle Phuket'};
@@ -35,8 +39,8 @@ describe('registerTrainingSession', function () {
 
         var session = {
             trainingDescription: ctrl.training,
-            beginning: filter('date')(ctrl.d1.dt, "dd/MM/yyyy"),
-            ending: filter('date')(ctrl.d2.dt, "dd/MM/yyyy"),
+            beginning: dateBeginning,
+            ending: dateEnding,
             beginningTime: ctrl.beginningHour,
             endingTime: ctrl.endHour,
             location: ctrl.trainingLocation.name
@@ -55,18 +59,41 @@ describe('registerTrainingSession', function () {
         expect(loc.path()).toBe('/RegisterTrainingSession');
     });
 
-    it('3) Enregistrement avec session déjà planifiée', function () {
+    it('3 Enregistrement avec une date invalide',function () {
         ctrl.training = TRAININGS[0];
         ctrl.d1.dt = new Date(2016, 5, 23);
-        ctrl.d2.dt = new Date(2016, 5, 25);
+        ctrl.checkDateValide(ctrl.d1);
+        var dateBeginning=filter('date')(ctrl.d1.dt, "dd/MM/yyyy");
+        expect(dateBeginning).toMatch(ctrl.regex.beginning);
+        var dateEnding="0304/2016";
+        ctrl.d2.dt = undefined;
+        ctrl.checkDateValide(ctrl.d2);
+        expect(dateEnding).not.toMatch(ctrl.regex.ending);
         ctrl.beginningHour = "08:00";
         ctrl.endHour = "08:00";
         var meetingRoom1 = {name: 'Salle Phuket'};
         ctrl.trainingLocation = meetingRoom1;
+        expect(ctrl.isFalseDate).toBeTruthy();
+        expect(ctrl.isFalseForm).toBeFalsy();
+    });
+
+    it('4 Enregistrement avec session déjà planifiée', function () {
+        ctrl.training = TRAININGS[0];
+        ctrl.d1.dt = new Date(2016, 5, 23);
+        var dateBeginning=filter('date')(ctrl.d1.dt, "dd/MM/yyyy");
+        expect(dateBeginning).toMatch(ctrl.regex.beginning);
+        ctrl.d2.dt = new Date(2016, 5, 25);
+        var dateEnding=filter('date')(ctrl.d2.dt, "dd/MM/yyyy");
+        expect(dateEnding).toMatch(ctrl.regex.ending);
+        ctrl.beginningHour = "08:00";
+        ctrl.endHour = "08:00";
+        var meetingRoom1 = {name: 'Salle Phuket'};
+        ctrl.trainingLocation = meetingRoom1;
+
         var session = {
             trainingDescription: ctrl.training,
-            beginning: filter('date')(ctrl.d1.dt, "dd/MM/yyyy"),
-            ending: filter('date')(ctrl.d2.dt, "dd/MM/yyyy"),
+            beginning: dateBeginning,
+            ending: dateEnding,
             beginningTime: ctrl.beginningHour,
             endingTime: ctrl.endHour,
             location: ctrl.trainingLocation.name
