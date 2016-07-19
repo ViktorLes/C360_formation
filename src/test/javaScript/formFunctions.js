6e7812b419b94900c53e7ac3240971c79b7d100d
Lecomte <TLE3473@viseo.net>
2016-07-19 17:05:40 +0200

    constructor form Mock pending

--- src/test/javaScript/formFunctions.js
+++ src/test/javaScript/formFunctions.js
@@ -16,3 +16,39 @@ function expectFormToBeFilled(form) {
     expect(form.$error.required).toBeUndefined();
     form.$invalid = false;
 }
+
+
+function Form(){
+
+    var form = {
+        $error: {},
+        $invalid: false,
+        $valid: true,
+        inputs: [],
+        inputsMessages: []
+    };
+    form.createInput = function(name, obj){
+        var input = {
+            type: 'text',
+            'ng-pattern': null,
+            'ng-focus' : null,
+            'ng-change' : null,
+            'ng-blur' : null,
+            required: false,
+            'ng-model' : null,
+            initialValue : "",
+            $invalid : false,
+            $valid : true,
+            $error : {}
+        };
+        for(var property in obj) {
+            if(input[property] !== undefined){
+                input[property] = obj[property];
+            }
+        }
+        inputs[name] = input;
+    };
+
+    return form;
+}
+
