8a4e86b7d503302f545ccfc47146adc40170dd4a
Lecomte <TLE3473@viseo.net>
2016-07-21 16:11:22 +0200

    refactoring test registerTrainingSpec avec le mock form

--- src/test/javaScript/mock/formAngularMockExpectations.js
+++ src/test/javaScript/mock/formAngularMockExpectations.js
@@ -2,8 +2,8 @@ function expectFormToBeComplete(form) {
     expect(form.$error.required).toBeUndefined();
 }
 
-function expectFormToHaveMissingFields(form, number){
-    expect(form.$error.required.length).toBe(number);
+function expectFormToHaveMissingFields(form, numberOfMissingFields){
+    expect(form.$error.required.length).toBe(numberOfMissingFields);
 }
 
 function expectFormToBeValid(form) {
