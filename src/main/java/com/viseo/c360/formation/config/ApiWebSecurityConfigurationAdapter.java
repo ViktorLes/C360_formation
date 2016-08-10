e297df3ca132fc15e4600956859b15cda36a0628
Lecomte <TLE3473@viseo.net>
2016-08-10 12:24:06 +0200

    config test pending

--- src/main/java/com/viseo/c360/formation/config/ApiWebSecurityConfigurationAdapter.java
+++ src/main/java/com/viseo/c360/formation/config/ApiWebSecurityConfigurationAdapter.java
@@ -1,12 +1,15 @@
 package com.viseo.c360.formation.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+//@Configuration
 public class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
 /*
     @Autowired
@@ -15,13 +18,23 @@ public class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAda
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
-*/
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.httpBasic().and()
+                .httpBasic().and()
                 .authorizeRequests()
-                .antMatchers("/apis**").permitAll();
-                //.anyRequest()
-                //.authenticated();
+                .antMatchers("/api/**")
+                .permitAll().anyRequest().authenticated();
     }
+
+    @Override
+    public void configure(WebSecurity web){
+        web
+                .ignoring()
+                .antMatchers("/api/collaborateurs");
+    }
+
+*/
+
 }
