39b0a33ecc66d7d94c6c5f350e51f4aca349f86e
BayrekMokni <mohamed-bayrek.mokni@viseo.com>
2016-09-22 17:12:37 +0200

    messge OK GF24

--- src/main/webapp/app/controller/registerTraining.js
+++ src/main/webapp/app/controller/registerTraining.js
@@ -1,88 +1,107 @@
 angular.module('controllers')
-    .controller('controllerRegisterTraining', ['$http', '$location', '$timeout','SelectTrainingService', function ($http, $location,$timeout,SelectTrainingService) {
+    .controller('controllerRegisterTraining', ['$http', '$location', '$timeout', 'SelectTrainingService',
+        function ($http, $location, $timeout, SelectTrainingService) {
 
-        var self = this;
-        self.regex = {};
-        self.isNewTrainingTitle = true;
-        self.isFalseForm = false;
-        self.isThereAnEmptyField = false;
-        self.isTrainingSaved=false;
-        /*** Recupération des regex **/
-        $http.get("api/formations/regex").then(function (data) {
-            self.regex.trainingTitle = new RegExp(data.data.TRAINING_TITLE);
-            self.regex.numberHalfDays = new RegExp(data.data.NUMBER_HALF_DAYS);
-        });
-        /*** Recupération des Thèmes **/
-        $http.get("api/themes").then(function (data) {
-            self.topicList = data.data;
-        });
-        /*** Recupération des formations **/
-        $http.get("api/formations").then(function (data) {
-            self.trainingList = data.data;
-        });
-
-        self.isErrorInputMessageDisplayed = function (inputForm, focus) {
-            return !inputForm.$error.required && inputForm.$invalid && !focus;
-        };
-
-        self.verifyForm = function (trainingForm) {
+            var self = this;
+            self.regex = {};
             self.isNewTrainingTitle = true;
             self.isFalseForm = false;
             self.isThereAnEmptyField = false;
-            self.isTrainingSaved=false;
-            if (trainingForm.$error.required) {
-                self.isThereAnEmptyField = true;
-                self.isFalseForm = false;
-            }
-            else if (trainingForm.$invalid) {
-                self.isFalseForm = true;
-                self.isThereAnEmptyField = false;
-            }
-            else {
-                self.saveAction();
-            }
-        };
+            self.isTrainingSaved = false;
+            /*** Recupération des regex **/
+            $http.get("api/formations/regex").then(function (data) {
+                self.regex.trainingTitle = new RegExp(data.data.TRAINING_TITLE);
+                self.regex.numberHalfDays = new RegExp(data.data.NUMBER_HALF_DAYS);
+            });
+            /*** Recupération des Thèmes **/
+            $http.get("api/themes").then(function (data) {
+                self.topicList = data.data;
+            });
+            /*** Recupération des formations **/
+            $http.get("api/formations").then(function (data) {
+                self.trainingList = data.data;
+            });
 
-        self.saveAction = function () {
-            self.training.trainingTitle = self.training.trainingTitle.replace(/ +/g, " ");
-            $http.post("api/formations", self.training).then(function (response) {
-                    self.trainingList.push(response.data);
-                    self.isTrainingSaved = true;
-                    self.training.trainingTitle = null;
-                    self.training.topicDescription = null;
-                    self.training.numberHalfDays = null;
-                    self.setConfirmationMessageTimOut();
+            self.isErrorInputMessageDisplayed = function (inputForm, focus) {
+                return !inputForm.$error.required && inputForm.$invalid && !focus;
+            };
+
+            self.verifyForm = function (trainingForm) {
+                self.isNewTrainingTitle = true;
+                self.isFalseForm = false;
+                self.isThereAnEmptyField = false;
+                self.isTrainingSaved = false;
+                if (trainingForm.$error.required) {
+                    self.isThereAnEmptyField = true;
+                    self.isFalseForm = false;
+                }
+                else if (trainingForm.$invalid) {
+                    self.isFalseForm = true;
+                    self.isThereAnEmptyField = false;
+                }
+                else {
+                    self.saveAction();
+                }
+            };
+
+            self.saveAction = function () {
+                self.training.trainingTitle = self.training.trainingTitle.replace(/ +/g, " ");
+                $http.post("api/formations", self.training).then(function (response) {
+                        self.trainingList.push(response.data);
+                        self.isTrainingSaved = true;
+                        self.training.trainingTitle = null;
+                        self.training.topicDescription = null;
+                        self.training.numberHalfDays = null;
+                        self.setConfirmationMessageTimOut();
+                    },
+                    function (error) {
+                        if (error.data.message === "trainingTitle") {
+                            self.isNewTrainingTitle = false;
+                        } else {
+                            console.error(error);
+                        }
+                    });
+            };
+
+            self.setConfirmationMessageTimOut = function () {
+                $timeout(function () {
+                    self.isTrainingSaved = false;
+                }, 3000);
+            };
+
+            var indexedTeams = [];
+            self.returnTrainingListToFilter = function () {
+                indexedTeams = [];
+                return self.trainingList;
+            };
+            self.filterTopic = function (training) {
+                var isNewTopic = indexedTeams.indexOf(training.topicDescription.name) == -1;
+                if (isNewTopic) indexedTeams.push(training.topicDescription.name);
+                return isNewTopic;
+            };
+            self.manageSession = function (training) {
+                SelectTrainingService.select(training);
+                $location.url("/ManageSession");
+            };
+        }])
+
+    .controller('InsideCtrl', function ($scope,$http,ngDialog) {
+        var self = $scope;
+        self.addTopic = function (topic) {
+            console.log("here: ",topic);
+            $http.post("api/themes", topic).then(function (response) {
+                    console.log("ajout theme", response.data);
+
+                    console.log("self.topicList: ", $scope.topicList);
+
+                    ngDialog.close();
                 },
                 function (error) {
-                    if (error.data.message === "trainingTitle") {
-                        self.isNewTrainingTitle = false;
-                    } else {
-                        console.error(error);
-                    }
+
                 });
-        };
+        }
 
-        self.setConfirmationMessageTimOut = function () {
-            $timeout(function () {
-                self.isTrainingSaved = false;
-            }, 3000);
-        };
-
-        var indexedTeams = [];
-        self.returnTrainingListToFilter = function () {
-            indexedTeams = [];
-            return self.trainingList;
-        };
-        self.filterTopic = function (training) {
-            var isNewTopic = indexedTeams.indexOf(training.topicDescription.name) == -1;
-            if (isNewTopic) indexedTeams.push(training.topicDescription.name);
-            return isNewTopic;
-        };
-        self.manageSession = function (training) {
-            SelectTrainingService.select(training);
-            $location.url("/ManageSession");
-        };
-    }])
+    })
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
